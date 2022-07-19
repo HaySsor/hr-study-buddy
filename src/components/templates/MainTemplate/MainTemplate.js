c6120c09be21125c89d93a7936e7fc7f47d82318
Przemek <Hayss007@gmail.com>
2022-07-19 10:09:32 +0200

    add searchbar component

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Navigation from 'components/organisms/Navigation/Navigation';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
 
+import styled from 'styled-components';
+
+const SearchBar = styled.div`
+  grid-row: 1/ 2;
+  grid-column: 2 / 3;
+  border: 1px solid ${({theme}) => theme.colors.darkPurple}
+
+  
+  `
 const MainTemplate = ({ children }) => {
   return (
     <Wrapper>
       <Navigation />
+      <SearchBar />
       {children}
     </Wrapper>
   );
