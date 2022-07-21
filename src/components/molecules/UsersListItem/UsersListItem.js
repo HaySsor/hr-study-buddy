8e49aca286c3f04392c74da2ac76dcc6a0699771
Przemek <Hayss007@gmail.com>
2022-07-21 11:12:29 +0200

    add "back-end" and send axios.get to read te student list add group buttons, and add search bar to finde students

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,26 +1,26 @@
-import React,{useContext} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
 import { StyledAverage, StyledInfo, Wrapper } from './UsersListItem.styles';
 import { UserShape } from 'types';
-import { UserContext } from 'providers/UsersProvider';
 
-const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) =>{
+const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => {
+  return (
+    <Wrapper>
+      <StyledAverage value={average}>{average}</StyledAverage>
+      <StyledInfo>
+        <p>
+          {name}
+          <DeleteButton />
+        </p>
+        <p>attendance: {attendance}</p>
+      </StyledInfo>
+    </Wrapper>
+  );
+};
 
-const {deleteUser} = useContext(UserContext)
-
-return(
-  <Wrapper>
-    <StyledAverage value={average}>{average}</StyledAverage>
-    <StyledInfo>
-      <p>
-        {name}
-        <DeleteButton onClick={() => deleteUser(name)} />
-      </p>
-      <p>attendance: {attendance}</p>
-    </StyledInfo>
-  </Wrapper>
-)
-}
+UsersListItem.propTypes = {
+  userData: PropTypes.shape(UserShape),
+};
 
 export default UsersListItem;
--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
+import { useParams } from 'react-router-dom';
 import { StyledList } from './UsersList.styles';
-import { UserShape } from 'types';
 import { Title } from 'components/atoms/Title/Title';
+import { useStudents } from 'hooks/useStudenst';
+import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 
-const UsersList = ({users}) => {
+const UsersList = () => {
+  const { id } = useParams();
+  const { students } = useStudents({ groupId: id });
+
   return (
     <>
       <Title>Students list</Title>
       <StyledList>
-        {users.map((userData) => (
-          <UsersListItem  key={userData.name} userData={userData} />
+        {students.map((userData) => (
+          <UsersListItem key={userData.name} userData={userData} />
         ))}
       </StyledList>
     </>
   );
-};
-
-UsersList.propTypes = {
-  users: PropTypes.arrayOf(PropTypes.shape(UserShape)),
-  deleteUser: PropTypes.func,
 };
 
 export default UsersList;
